81080bee899cf6179a608cca06be8492333af2ce
Akeem <akeem.mighty@gmail.com>
2022-03-18 14:49:52 -0400

    update shopping class

--- product.py
+++ product.py
@@ -1,5 +1,3 @@
 class Product:
-    def __init__(self, name, price, category):
-        self.name = name
-        self.price = price
-        self.category = category
+    def __init__(self):
+        pass
