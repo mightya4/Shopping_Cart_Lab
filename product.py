9927d8852222461c1b8e11f16c6a6e89f5691d9f
Akeem <akeem.mighty@gmail.com>
2022-03-18 14:51:36 -0400

    update product class with initial variables

--- product.py
+++ product.py
@@ -1,3 +1,5 @@
 class Product:
-    def __init__(self):
-        pass
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
---
+++ shopping_cart.py
@@ -0,0 +1,2 @@
+class ShoppingCart:
+    def __init__(self):
